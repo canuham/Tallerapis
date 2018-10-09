d6ba1193dc064e7fdbe4abd117c44bf205810cee
Nury Florez <nuryperez711@gmail.com>
2018-10-09 18:51:38 -0500

    Agregar archivos de proyecto.

---
+++ TallerAPI.Xamarin/TallerAPI.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TallerAPI.Xamarin.Models;
+
+namespace TallerAPI.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
